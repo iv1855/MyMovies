8dbcbab8f83723486eaa1bc5cef8feaef6578e02
K <rp901@yandex.ru>
2020-07-30 17:48:00 +0300

    2 cc

--- app/src/main/java/com/example/mymovies/MainActivity.java
+++ app/src/main/java/com/example/mymovies/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.mymovies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.mymovies.utils.NetworkUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String url = NetworkUtils.buildURL(NetworkUtils.POPULARITY, 1).toString();
+        Log.i("MyResult", url);
     }
 }
--- app/src/main/java/com/example/mymovies/utils/NetworkUtils.java
+++ app/src/main/java/com/example/mymovies/utils/NetworkUtils.java
@@ -2,6 +2,7 @@ package com.example.mymovies.utils;
 
 import android.net.Uri;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class NetworkUtils {
@@ -10,7 +11,7 @@ public class NetworkUtils {
 
     public static final String PARAMS_API_KEY = "api_key";
     public static final String PARAMS_LANGUAGE = "language";
-    public static final String PARAMS_SORT_BY = "api_key";
+    public static final String PARAMS_SORT_BY = "popularity.desc";
     public static final String PARAMS_PAGE = "page";
 
     private static final String API_KEY = "44da32d2561451b0acf05688fa5f527a";
@@ -18,11 +19,30 @@ public class NetworkUtils {
     public static final String SORT_BY_POPULARITY = "popularity.desc";
     public static final String SORT_BY_TOP_RATED = "vote_average.desc";
 
-    public static final int  POPULARITY = 0;
+    public static final int POPULARITY = 0;
     public static final int TOP_RATED = 1;
 
-    public static URL buildURL() {
+    public static URL buildURL(int sortBy, int page) {
+        URL result = null;
+        String methodOfSort;
+        if (sortBy == POPULARITY) {
+            methodOfSort = SORT_BY_POPULARITY;
+        } else {
+            methodOfSort = SORT_BY_TOP_RATED;
+        }
         Uri uri = Uri.parse(BASE_URL).buildUpon()
+                .appendQueryParameter(PARAMS_API_KEY, API_KEY)
+                .appendQueryParameter(PARAMS_LANGUAGE, LANGUAGE_VALUE)
+                .appendQueryParameter(PARAMS_SORT_BY, methodOfSort)
+                .appendQueryParameter(PARAMS_PAGE, Integer.toString(page))
+                .build();
+        try {
+            result = new URL(uri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return result;
+
     }
 
 }
