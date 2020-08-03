ebce837d433bf5c5bf803257fc026973113163b2
K <rp901@yandex.ru>
2020-08-03 12:34:52 +0300

    n2 commit

--- app/src/main/java/com/example/mymovies/MovieAdapter.java
+++ app/src/main/java/com/example/mymovies/MovieAdapter.java
@@ -1,18 +1,61 @@
 package com.example.mymovies;
 
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class MovieAdapter {
+import com.example.mymovies.data.Movie;
+
+import java.util.ArrayList;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+
+    private ArrayList<Movie> movies;
+
+    public MovieAdapter() {
+        movies = new ArrayList<>();
+    }
+
+    @NonNull
+    @Override
+    public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
     class MovieViewHolder extends RecyclerView.ViewHolder {
 
         private ImageView imageViewSmallPoster;
 
         public MovieViewHolder(@NonNull View itemView) {
             super(itemView);
+            imageViewSmallPoster = itemView.findViewById(R.id.imageViewSmallPoster);
         }
+    }
+
+    public void setMovies(ArrayList<Movie> movies) {
+        this.movies = movies;
+        notifyDataSetChanged();
+    }
+
+    private void addMovies(ArrayList<Movie> movies) {
+        this.movies.addAll(movies);
+        notifyDataSetChanged();
+    }
+
+    public ArrayList<Movie> getMovies() {
+        return movies;
     }
 }
