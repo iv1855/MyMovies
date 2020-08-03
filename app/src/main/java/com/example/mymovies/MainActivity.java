2d6e333c6d0eb2ca9e2d3a19ef15724c76187860
K <rp901@yandex.ru>
2020-08-03 10:28:53 +0300

    2 cc

--- app/src/main/java/com/example/mymovies/MainActivity.java
+++ app/src/main/java/com/example/mymovies/MainActivity.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.example.mymovies.data.Movie;
+import com.example.mymovies.utils.JSONUtils;
 import com.example.mymovies.utils.NetworkUtils;
 
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +20,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(NetworkUtils.POPULARITY, 5);
+        ArrayList<Movie> movies = JSONUtils.getMoviesFromJSON(jsonObject);
+        StringBuilder builder = new StringBuilder();
+        for (Movie movie: movies) {
+            builder.append(movie.getTitle()).append("\n");
+        }
+        Log.i("MyResult", builder.toString());
+
     }
 }
--- app/src/main/java/com/example/mymovies/utils/JSONUtils.java
+++ app/src/main/java/com/example/mymovies/utils/JSONUtils.java
@@ -16,7 +16,7 @@ public class JSONUtils {
     private static final String KEY_TITLE = "title";
     private static final String KEY_ORIGINAL_TITLE = "original_title";
     private static final String KEY_OVERVIEW = "overview";
-    private static final String KEY_POSTER_PASS = "poster_pass";
+    private static final String KEY_POSTER_PASS = "poster_path";
     private static final String KEY_BACKDROP_PATH = "backdrop_path";
     private static final String KEY_VOTE_AVERAGE = "vote_average";
     private static final String KEY_RELEASE_DATE = "release_date";
